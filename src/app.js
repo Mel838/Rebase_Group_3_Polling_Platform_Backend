691b8519c4a855600ae41af9f9a14504b1df7326
Salah Mel-Catherine Atuma <melcatherinesalah@gmail.com>
2025-09-04 14:32:39 +0100

    added cors and helmet for protection, added winston and logger function for logs

--- src/app.js
+++ src/app.js
@@ -1,18 +1,20 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+import helmet from "helmet";
+import cors from "cors";
 
 import indexRouter from "./routes/index.js";
 import usersRouter from "./routes/users.js";
 
-const app = express();
-
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(helmet())
+app.use(cors({
+  origin: ["http://localhost:4000", "frontendapp.vercel.app"]
+}))
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-export default app;
---
+++ src/utils/logger.js
@@ -0,0 +1,53 @@
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+import winston from "winston"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const logDir = path.join(__dirname, "../logs")
+
+export const logger = winston.createLogger({
+  level: "debug", // Set logging level
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.errors({ stack: true }),
+    winston.format.json()
+  ),
+  defaultMeta: { service: 'todos-api' },
+  transports: [
+    new winston.transports.File({
+      filename: path.join(logDir, "error.log"),
+      level: 'error',
+      maxsize: 5242880
+    }),
+    new winston.transports.File({
+      filename: path.join(logDir, "app.log"),
+      level: "info",
+      maxsize: 5242880 
+    })
+  ],
+  exceptionHandlers: [
+    new winston.transports.File({ 
+      filename: path.join(logDir, 'exceptions.log') 
+    })
+  ],
+  rejectionHandlers: [
+    new winston.transports.File({ 
+      filename: path.join(logDir, 'rejections.log') 
+    })
+  ],
+  exitOnError: false
+})
+
+// Add console transport for non-production environments
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.timestamp(),
+      winston.format.align()
+    ),
+    level: "debug"
+  }))
+}
